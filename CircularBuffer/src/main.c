5180d91e498f3d8df47fd332ebd4663685f00f9d
Alex Tran <alex.tran1502@gmail.com>
2020-01-26 23:47:27 -0600

    Finished write/read for CircularBuffer, adding structure and union

--- CircularBuffer/src/main.c
+++ CircularBuffer/src/main.c
@@ -1,15 +1,17 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "circular_buffer.h"
 
+#define BUFFER_SIZE 10
 /* Structure */
 typedef struct
 {
-    char *buffer;
-    size_t write_pointer;
-    size_t read_pointer;
-    size_t buffer_length;
+    char buffer[BUFFER_SIZE];
+    uint8_t write_pointer;
+    uint8_t read_pointer;
+    uint8_t buffer_length;
 } circular_buf_t;
 
 typedef enum
@@ -21,39 +23,87 @@ typedef enum
 } BUFFER_STATUS;
 
 /* Prototype Functions */
-BUFFER_STATUS init_buffer(circular_buf_t *buffer, size_t buffer_length);
+BUFFER_STATUS init_buffer(circular_buf_t *buffer, uint8_t buffer_length);
+BUFFER_STATUS buffer_read(circular_buf_t *buffer, char *data);
+BUFFER_STATUS init_buffer(circular_buf_t *buffer, uint8_t buffer_length);
 void print_buffer_info(circular_buf_t *buffer);
 
 /* API Implementation */
-BUFFER_STATUS init_buffer(circular_buf_t *buffer, size_t buffer_length)
+BUFFER_STATUS init_buffer(circular_buf_t *buffer, uint8_t buffer_length)
 {
     printf("Start Init Buffer\n");
 
     assert(buffer != NULL);
-    buffer->buffer = (char *)malloc(buffer_length * sizeof(char));
-    for (int i = 0; i < buffer_length; i++)
-    {
-        buffer->buffer[i] = 'a' + i;
-    }
 
     buffer->write_pointer = 0;
     buffer->read_pointer = 0;
     buffer->buffer_length = buffer_length;
+
+    for (int i = 0; i < buffer_length; i++)
+    {
+        buffer->buffer[i] = 0;
+    }
+
     printf("Done Init Buffer\n");
 
     return BUFFER_OK;
 }
 
+BUFFER_STATUS buffer_write(circular_buf_t *buffer, uint8_t data)
+{
+
+    if (buffer == NULL)
+    {
+        return BUFFER_ERROR;
+    }
+
+    /* Check if buffer is full */
+    uint8_t next_index = (buffer->write_pointer + 1) % buffer->buffer_length;
+
+    if (next_index == buffer->read_pointer)
+    {
+        printf("Buffer is Full %d\n", next_index);
+        return BUFFER_FULL;
+    }
+
+    buffer->buffer[buffer->write_pointer] = data;
+    buffer->write_pointer = next_index;
+
+    return BUFFER_OK;
+}
+
+BUFFER_STATUS buffer_read(circular_buf_t *buffer, char *data)
+{
+    printf("-------------------- READ \n");
+    uint8_t next_index = (buffer->read_pointer + 1) % buffer->buffer_length;
+
+    if (buffer == NULL)
+    {
+        return BUFFER_ERROR;
+    }
+
+    /* Check if buffer is EMPTY */
+    if (buffer->read_pointer == buffer->write_pointer)
+    {
+        printf("BUFFER EMPTY\n");
+        return BUFFER_EMPTY;
+    }
+
+    *data = buffer->buffer[buffer->read_pointer];
+    buffer->read_pointer = next_index;
+
+    return BUFFER_OK;
+}
 int main()
 {
     BUFFER_STATUS status;
 
     circular_buf_t myBuffer;
     printf("Buffer address: %p\n", &myBuffer);
-    // print_buffer_info(&myBuffer);
+    uint8_t data_return;
 
     /* Initialization of buffer */
-    status = init_buffer(&myBuffer, 10);
+    status = init_buffer(&myBuffer, BUFFER_SIZE);
     if (status != BUFFER_OK)
     {
         printf("Init Buffer Failed\n");
@@ -61,6 +111,34 @@ int main()
 
     print_buffer_info(&myBuffer);
 
+    buffer_write(&myBuffer, 'd');
+    print_buffer_info(&myBuffer);
+
+    buffer_write(&myBuffer, 'e');
+    print_buffer_info(&myBuffer);
+
+    buffer_write(&myBuffer, 'a');
+    print_buffer_info(&myBuffer);
+
+    
+    buffer_read(&myBuffer, &data_return);
+    print_buffer_info(&myBuffer);
+    printf("DATA %c\n", data_return);
+
+    buffer_read(&myBuffer, &data_return);
+    print_buffer_info(&myBuffer);
+    printf("DATA %c\n", data_return);
+
+    buffer_read(&myBuffer, &data_return);
+    print_buffer_info(&myBuffer);
+    printf("DATA %c\n", data_return);
+
+    buffer_read(&myBuffer, &data_return);
+    print_buffer_info(&myBuffer);
+    printf("DATA %c\n", data_return);
+
+    buffer_write(&myBuffer, 'd');
+    print_buffer_info(&myBuffer);
     return 0;
 }
 
@@ -70,11 +148,26 @@ void print_buffer_info(circular_buf_t *buffer)
 
     for (int i = 0; i < buffer->buffer_length; i++)
     {
-        printf("Buffer[%d]: %c\n", i, buffer->buffer[i]);
+
+        printf("|\t Buffer[%d]: %c[%d] \t|", i, buffer->buffer[i], buffer->buffer[i]);
+
+        if (buffer->write_pointer == i)
+        {
+            printf(" <= WRITE|HEAD \t");
+        }
+
+        if (buffer->read_pointer == i)
+        {
+            printf(" <= READ|TAIL \n");
+        }
+        else
+        {
+            printf("\n");
+        }
     }
 
-    printf("write pointer: %ld\n", buffer->write_pointer);
-    printf("read pointer: %ld\n", buffer->read_pointer);
-    printf("buffer length: %ld\n", buffer->buffer_length);
+    printf("write pointer: %d\n", buffer->write_pointer);
+    printf("read pointer: %d\n", buffer->read_pointer);
+    printf("buffer length: %d\n", buffer->buffer_length);
     printf("----------**----------\n\n");
 }
